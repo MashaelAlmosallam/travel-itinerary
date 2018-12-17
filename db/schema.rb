da4a6f0c72015c9cee9d1fbbdd1b8af5b6d40b33
razan <almusarazan@gmail.com>
2018-12-17 10:19:32 +0300

    second day

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_12_16_103602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "places", force: :cascade do |t|
+  create_table "sightseeings", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "location"
